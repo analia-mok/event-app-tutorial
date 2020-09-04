8b486dce6a4f8cf759c90615a52c1243ab18e10a
Analia Mok <analiam319@gmail.com>
2020-09-04 11:52:30 -0400

    Finished front-end connect of tutorial

---
+++ e-vents-front/src/templates/Event.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <h1>{{ $page.event.title }}</h1>
+    <img :src="`http://localhost:1337${$page.event.image}`" :alt="$page.event.title">
+    <p>{{ $page.event.description }}</p>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+	event(id: $id) {
+    id,
+    title,
+    description,
+    price,
+    duration,
+    date,
+    image
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.event.title
+    }
+  }
+}
+</script>
